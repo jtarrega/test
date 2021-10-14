bc5abcbf9dc61f5397719a51e8860e982c43a04c
jtarrega <fjtarrega@gmail.com>
2021-10-14 13:00:00 +0200

    he hecho cambios

--- test.php
+++ test.php
@@ -1,12 +1,15 @@
 <html>
+
 <head>
-<title>Test PHP virtual host</title>
+    <title>Test PHP virtual host</title>
 </head>
+
 <body>
 
-<?php
-echo '<h1>Hola mundo con Host Virtual</h1>';
+    <?php
+    echo '<h1>Hola mundo con Host Virtual cambiamos el texto</h1>';
 
-?>
+    ?>
 </body>
+
 </html>
